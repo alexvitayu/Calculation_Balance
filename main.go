d0124aab1c6c483e9e3faa33952b043c9617fe96
alexvitayu <alexvitayu1212@gmail.com>
2024-10-24 20:34:02 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+// В цикле спрашиваем ввод транзакций: -10, 10, 40.5
+// Добавим каждую в массив транзакций
+// Вывести массив
+// Вывести сумму баланса в консоль
+
+func main() {
+	transactions := []float64{}
+	for {
+		transaction := inputData()
+		if transaction == 0 {
+			break
+		}
+		transactions = append(transactions, transaction)
+	}
+	summ := calculateBalance(transactions)
+	fmt.Println(transactions)
+	//fmt.Printf("Ваш баланс составляет: %.1f рублей", summ)
+	fmt.Println(summ)
+
+}
+func inputData() float64 {
+	var data float64
+	fmt.Println("Введите транзакцию или нажмите n для выхода:")
+	fmt.Scan(&data)
+	return data
+}
+func calculateBalance(transactions []float64) float64 {
+	balance := 0.0
+	for _, value := range transactions {
+		balance = balance + value
+	}
+	return balance
+}
