74f2350fd24454802ce5d4fd4895a7e2f85173e8
alexvitayu <alexvitayu1212@gmail.com>
2024-11-06 19:07:44 +0300

    feat: my finishing version

--- main.go
+++ main.go
@@ -8,32 +8,30 @@ import "fmt"
 // Вывести сумму баланса в консоль
 
 func main() {
-transactions := []float64{}
-var data float64
-for {
-data = inputTransactions()
-if data == 0 {
-	break
-}
-transactions = append(transactions, data)
-}	
-total := totalTransactions(transactions)
-fmt.Println(transactions)
-fmt.Printf("Сумма ваших транзакций составляет: %.2f", total)
-}
-
-func inputTransactions ()  float64 {
-	var data float64
-fmt.Println("Введите новую транзакцию:")
-fmt.Scan(&data)
-return data
-}
-
-func totalTransactions (transactions []float64) float64 {
-	data := 0.0
-	for _,value := range transactions {
-		data += value
+	transactions := []float64{}
+	for {
+		transaction := inputData()
+		if transaction == 0 {
+			break
+		}
+		transactions = append(transactions, transaction)
 	}
-	return data
+	summ := calculateBalance(transactions)
+	fmt.Println(transactions)
+	//fmt.Printf("Ваш баланс составляет: %.1f рублей", summ)
+	fmt.Println(summ)
 
+}
+func inputData() float64 {
+	var data float64
+	fmt.Println("Введите транзакцию или нажмите n для выхода:")
+	fmt.Scan(&data)
+	return data
+}
+func calculateBalance(transactions []float64) float64 {
+	balance := 0.0
+	for _, value := range transactions {
+		balance = balance + value
+	}
+	return balance
 }
